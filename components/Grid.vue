f4fd91ee70cf1ee2daf1f012997a5f910feca586
Kristians Murds <murdskristians@gmail.com>
2020-01-19 21:23:17 +0200

    add all info and pixtures for grid

---
+++ components/Grid.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1 class="grid-title">display: grid;</h1>
+
+    <section class="main">
+      <section class="ai">
+        <h2>Ai</h2>
+        <h2>align-items: y-axis</h2>
+        <img src="~/assets/grid-ver.png" alt="Grid vertical positions" />
+        <h2>align-self</h2>
+      </section>
+
+      <section class="jc">
+        <h2>Jc</h2>
+        <h2>justify-items: x-axis</h2>
+        <img src="~/assets/grid-hor.png" alt="Grid horizontal positions" />
+        <h2>justify-self</h2>
+      </section>
+    </section>
+
+    <section class="additional">
+      <p>grid-template-(rows/columns): 1fr, 2fr...;(fr,px,%,auto)</p>
+      <p>
+        grid-template-areas: "head head head
+        nav main main
+        nav main main"
+      </p>
+      <p>grid-area: head/nav/main;</p>
+      <p>
+        align-items: start;
+        justify-items: center;
+      </p>
+      <p>place-items: start center;</p>
+    </section>
+
+    <h3>ALL</h3>
+    <h3>ONLY ONE</h3>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css" scoped>
+.grid-title {
+  position: relative;
+  text-align: center;
+  margin: 2vh 0 2vh 0;
+}
+
+.ai,
+.jc {
+  display: flex;
+  place-items: center;
+  place-content: space-around;
+}
+
+.additional {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,28 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        FlexGridCheatSheet
-      </h1>
-      <h2 class="subtitle">
-        This will be my cheat-sheet for CSS flex and grid.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <grid />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Grid from '~/components/Grid.vue'
 
 export default {
   components: {
-    Logo
+    Grid
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
